c745f2cd7f3bea4284aaba67cb16e720924b507e
NAD3R <naderdeghaili@gmail.com>
2025-11-11 18:00:09 +0100

    pointer cards

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,6 +1,7 @@
 const c1 = document.querySelectorAll(".album")
 
 c1.forEach((card) => {
+  card.style.cursor = "pointer"
   card.addEventListener("click", () => {
     const albumId = card.dataset.albumId //prendo l'id
     window.location.assign(`album.html?id=${albumId}`) //apro album.html con l'id dell'album

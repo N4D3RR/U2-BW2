24c5a684bc25d7dda998f8d42cc5263f36276743
NAD3R <naderdeghaili@gmail.com>
2025-11-13 09:15:44 +0100

    updated search func

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -20,22 +20,32 @@ const search = function () {
     })
     .then((data) => {
       console.log(data.data)
+      result.innerHTML = ""
       data.data.slice(0, 10).forEach((track) => {
         result.innerHTML += `<div class="card mb-3">
-  <div class="row g-0 ">
-    <div class="col-md-4">
-      <img src="${track.album.cover}" class="img-fluid rounded-start h-100" alt="Canzone foto">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">${track.title}</h5>
-        <p class="card-text">${track.artist.name}</p>
-        <p class="card-text "><small class="text-body-secondary">${track.album.title}</small></p>
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src="${track.album.cover}" class="img-fluid rounded-start h-100" alt="${track.title}">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">${track.title}</h5>
+          <p class="card-text">
+            <a href="artist.html?id=${track.artist.id}" class="text-decoration-none text-dark fw-semibold">
+              ${track.artist.name}
+            </a>
+          </p>
+          <p class="card-text">
+            <small class="text-body-secondary">
+              <a href="album.html?id=${track.album.id}" class="text-decoration-none text-secondary">
+                ${track.album.title}
+              </a>
+            </small>
+          </p>
+        </div>
       </div>
     </div>
-  </div>
-  </div>
-</>`
+  </div>`
       })
     })
     .catch((err) => {
